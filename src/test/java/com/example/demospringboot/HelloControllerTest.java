eac4d15a6d582245abfa60331c27b9c3bcad7906
sara <sara.tahabik@gmail.com>
2020-08-27 16:26:31 +0200

    Adding some TUs

---
+++ src/test/java/com/example/demospringboot/HelloControllerTest.java
@@ -0,0 +1,36 @@
+package com.example.demospringboot;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+    }
+    @Test
+    public void getHelloFromCustomPath() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/customPath").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Greetings from Spring Boot!")))
+                .andExpect(content().string(containsString("customPath")));
+    }
+}
